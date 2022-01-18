1f55ef36b5659cb78f71549ae84a99648e53dee5
cookie-god <cookie-god@softsquared.com>
2022-01-18 19:26:56 +0900

    🎉 프로젝트 시작

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filters';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  // cats/
+  @Get()
+  @UseFilters(HttpExceptionFilter)
+  getAllCat() {
+    throw new HttpException('api is broken', 401);
+    return 'all cats';
+  }
+
+  // cats/:id
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updateParatialCat() {
+    return 'update partial cat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
