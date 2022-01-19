d67639c867b2ec9803300ebaa52fbbee4ba8e610
cookie-god <lion0193@gmail.com>
2022-01-19 21:28:31 +0900

    ✨ baseResponse 추가

--- config/baseResponse.utils.ts
+++ config/baseResponse.utils.ts
@@ -6,4 +6,7 @@ export const baseResponse = {
     code: 2000,
     message: '아이디가 없습니다.',
   },
+
+  NON_EXIST_EMAIL: { isSuccess: false, code: 2002, message: '이메일을 확인해주세요.' },
+  NON_MATCH_PASSWORD: { isSuccess: false, code: 2003, message: '비밀번호가 일치하지 않습니다.' },
 };
