b3a9e014fbbf53ed1b95c7a782ed151b7a9f2d30
cookie-god <cookie-god@softsquared.com>
2022-02-27 17:06:38 +0900

    🐛 프리티어 제거

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { secret } from "config/secret";
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from "@nestjs/swagger";
-import * as expressBasicAuth from "express-basic-auth";
+import { secret } from 'config/secret';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(
-    ["/docs", "/docs-json"],
+    ['/docs', '/docs-json'],
     expressBasicAuth({
       challenge: true,
       users: {
         cookie: secret.swagger_password,
       },
-    })
+    }),
   );
 
   const config = new DocumentBuilder()
-    .setTitle("Nestjs 템플릿")
-    .setDescription("템플릿 스웨거입니다.")
-    .setVersion("1.0.0")
+    .setTitle('Nestjs 템플릿')
+    .setDescription('템플릿 스웨거입니다.')
+    .setVersion('1.0.0')
     .build();
 
   const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("docs", app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3030);
 }
