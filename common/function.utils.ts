bec5077af003854ac60664207e70c43366c87b26
cookie-god <cookie-god@softsquared.com>
2022-05-29 10:16:52 +0900

    ✨  권한체크 모듈화 완성

--- common/function.utils.ts
+++ common/function.utils.ts
@@ -1,7 +1,26 @@
 import { HttpException } from '@nestjs/common';
 import { response } from 'config/response.utils';
 
+/**
+ * description : response를 만들어 주는 함수, result에 들어갈 것이 없다면 undefined 입력해야함
+ * @param response
+ * @param data
+ * @returns object
+ */
 export function makeResponse(response: any, data: any | any[] | undefined) {
   response.result = data;
   return response;
+}
+
+/**
+ * description : 권한 체크 함수
+ * @param authority
+ * @param list
+ * @returns boolean
+ */
+export function ApiAuthorityCheck(authority: string, list: string[]) {
+  if (list.indexOf(authority) === -1) {
+    return false;
+  }
+  return true;
 }
